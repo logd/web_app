d72b2fdfda53aa476d44ae7c03a7d081c962f7f9
Anders Ramsay <andersr@gmail.com>
2016-04-22 12:31:20 -0400

    push main content below app header

--- app/imports/pages/homepage.jsx
+++ app/imports/pages/homepage.jsx
@@ -28,11 +28,7 @@ export class Homepage extends React.Component {
 
   render() {
 
-    return <div className="app-container">
-      <SingleFieldSubmit placeholder="New Note..." handleSubmit={this.handleCreateNote} />
-      <div>
-        <NotesListContainer />
-      </div>
-    </div>     
+    return <div id="main-content"> <NotesListContainer /> </div>     
   }
 }
+// <SingleFieldSubmit placeholder="New Note..." handleSubmit={this.handleCreateNote} />
